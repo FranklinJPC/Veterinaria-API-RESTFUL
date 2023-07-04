8b0c98907d055a79853584791db9ed81771542a7
FranklinJPC <franklin.patino@epn.edu.ec>
2023-07-04 17:32:31 -0500

    Uliimos agregados al modulo de veterinarios

--- src/routers/veterinario.routes.js
+++ src/routers/veterinario.routes.js
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import verificarAutenticacion from '../middlewares/autentication.js';
 const router = Router()
 import{
     login,
@@ -14,6 +15,7 @@ import{
     nuevoPassword,
 } from "../controllers/veterinario.controller.js";
 
+/* -------------- Rutas Publicas ---------------------- */
 router.post("/login", login);
 router.post("/registro", registro);
 router.get("/confirmar/:token", confirmEmail);
@@ -22,10 +24,11 @@ router.get("/recuperar-password", recuperarPassword);
 router.get("/recuperar-password/:token", comprobarTokenPasword);
 router.post("/nuevo-password/:token", nuevoPassword);
 
-router.get("/perfil", perfil);
-router.put('/veterinario/actualizarpassword',actualizarPassword)
-router.get("/veterinario/:id", detalleVeterinario);
-router.put("/veterinario/:id", actualizarPerfil);
+/* -------------- Rutas Privadas ---------------------- */
+router.get("/perfil", verificarAutenticacion, perfil);
+router.put('/veterinario/actualizarpassword', verificarAutenticacion, actualizarPassword)
+router.get("/veterinario/:id", verificarAutenticacion, detalleVeterinario);
+router.put("/veterinario/:id", verificarAutenticacion, actualizarPerfil);
 
 
 export default router
